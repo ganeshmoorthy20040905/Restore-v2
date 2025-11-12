0acdd9866759e76bbaf2b6ce0e09dfad277b29a4
ganeshamoorthy <ganeshmoorthy200409052gamil.com>
2025-11-12 12:14:18 +0530

    pre publish updates

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt => 
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.    UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
 builder.Services.AddTransient<ExceptionMiddleware>();
@@ -20,10 +20,6 @@ builder.Services.AddScoped<PaymentService>();
 builder.Services.AddIdentityApiEndpoints<User>(opt =>
 {
  opt.User.RequireUniqueEmail = true;
-
-   // opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
-    //opt.Lockout.MaxFailedAccessAttempts = 5;
-    //opt.Lockout.AllowedForNewUsers = true;
 })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<StoreContext>();
@@ -32,6 +28,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseCors(opt => 
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:3000");
@@ -42,7 +42,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<User>(); // api/login
+app.MapFallbackToController("Index", "Fallback");
 
-DbInitializer.InitDb(app);
+await DbInitializer.InitDb(app);
 
 app.Run();
